7ef3b933489bffb08690702aec601614bfdb0ce0
Kenny <kharvey@spartaglobal.com>
2022-07-20 12:24:03 +0100

    Added exception handling for negatives and overflow

--- RecursionFactorial/RecursionFactorialApp/Program.cs
+++ RecursionFactorial/RecursionFactorialApp/Program.cs
@@ -9,10 +9,15 @@
 
         public static int RecFactorial(int num)
         {
-            if (num <= 1) return 1;
-            int prev = RecFactorial(num - 1);
-            int factorial = num * prev;
-            return factorial;
+            checked
+            {
+                if (num < 0) throw new ArgumentOutOfRangeException("Cannot be a negative number");
+                if (num <= 1) return 1;
+                int prev = RecFactorial(num - 1);
+                int factorial = num * prev;
+                return factorial;
+            }
+
         }
     }
 }
